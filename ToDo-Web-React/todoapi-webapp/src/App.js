6d856ce7b5b91bf7945480d981e8db94a37119b0
L-Luca6028 <luca.walkner@htl-saalfelden.at>
2023-05-16 09:16:43 +0200

    React-App

---
+++ ToDo-Web-React/todoapi-webapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>Hello World</p>      
+    </div>
+  );
+}
+
+export default App;
